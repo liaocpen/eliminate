bf927ab613c5537a48daa71a128e3eaa8a62ffaf
Liao_Cpen <wcpen@qq.com>
2014-01-14 09:58:35 +0800

    no message

---
+++ eliminate/eliminate/Classes/Scene/DotGameScene.h
@@ -0,0 +1,16 @@
+//
+//  DotGameScene.h
+//  eliminate
+//
+//  Created by Liao_Cpen on 14-1-13.
+//  Copyright 2014年 Liao_Cpen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "cocos2d.h"
+
+@interface DotGameScene : CCScene {
+    
+}
+
+@end
---
+++ eliminate/eliminate/Classes/Scene/DotGameScene.m
@@ -0,0 +1,14 @@
+//
+//  DotGameScene.m
+//  eliminate
+//
+//  Created by Liao_Cpen on 14-1-13.
+//  Copyright 2014年 Liao_Cpen. All rights reserved.
+//
+
+#import "DotGameScene.h"
+
+
+@implementation DotGameScene
+
+@end
